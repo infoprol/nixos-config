a2cc549c6be893c9e2986de6538855eb9ce02e90
infoprol <infoprol@icloud.com>
2025-09-15 10:57:14 -0500

    home-manager added and integrated with flakes

--- flake.nix
+++ flake.nix
@@ -5,24 +5,34 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-25.05";
     };
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     #unstable = {
     #  url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     #};
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
         lib = nixpkgs.lib;
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       albemuth = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];      
-
+        inherit system;
+	modules = [ ./configuration.nix ];      
       };
 
 
 
+    };
+    homeConfigurations = {
+      geezus = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home.nix ];
+      };
     };
   };
 }
