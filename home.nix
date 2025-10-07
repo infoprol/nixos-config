c8b421db12c13cc0da27a358d3933d97c583328c
infoprol <infoprol@icloud.com>
2025-10-07 01:29:54 -0500

    nvm env vars (but no .nvm)

--- home.nix
+++ home.nix
@@ -123,10 +123,16 @@
     enable = true;
     syntaxHighlighting.enable = true;
     #autosuggestions.enable = true;
+    initContent = ''      
+      	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    '';
     oh-my-zsh = {
       enable = true;
       #theme = "fino-time";
       theme = "half-life";
+  
       plugins = [
         "git"
         "history"
